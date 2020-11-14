f8c6b97da925ef9e4dbc7c34921a8800e1a84420
Mark Martynov <mark.martynov@yahoo.com>
2020-11-14 14:56:31 +0000

    Add fib example

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,40 @@ import Language.Befunge.TH
 main :: IO ()
 main = do
   putStrLn "Befunge qusi quoter tests:"
-  putStrLn tstCountQQ
-  putStrLn tstHelloWorldQQ
-  putStrLn tst
+  putStrLn countStr
+  putStrLn helloWorldStr
+  putStrLn fibStr
 
-tstCountQQ :: String
-tstCountQQ =
+countStr :: String
+countStr =
   [befunge|
           |>987v>.v |
           |v456<  : |
           |>321 ^ _@|]
 
-tstHelloWorldQQ :: String
-tstHelloWorldQQ =
+helloWorldStr :: String
+helloWorldStr =
   [befunge|
           |>25*"!dlroW olleH":v |
           |                v:,_@|
           |                >  ^ |]
 
+fibStr :: String
+fibStr =
+  [befunge|
+          |62*1+v>01p001>+v>\:02p\:02gv|
+          |     0       ^             <|
+          |     .         :p           |
+          |     "         .1           |
+          |        v 0," "<0           |
+          |     "  >1g12-+:|           |
+          |     ,          @           |
+          |     >^                     |]
+
 tst :: String
 tst =
   [befunge|
-        |2>:3g" "-!v\  g30          <           |
-        | |!`"&":+1_:.:03p>03g+:"&"`|           |
-        | @               ^  p3\" ":<           |
-        |2 2345678901234567890123456789012345678|]
+          |2>:3g" "-!v\  g30          <           |
+          | |!`"&":+1_:.:03p>03g+:"&"`|           |
+          | @               ^  p3\" ":<           |
+          |2 2345678901234567890123456789012345678|]
